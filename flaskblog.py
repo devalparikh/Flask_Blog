0a38e22d633c246df1174d683010e8c2afa60b4b
Deval Parikh <devalpp@gmail.com>
2018-06-25 20:57:52 -0400

    Layout HTML
    HTML pages for the home and about using layout html and block conent

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+#Mock Database Response
+posts = [
+
+    {
+        'author': 'Deval Parikh',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 25, 2018'
+    },
+    {
+        'author': 'Bob Builder',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 28, 2018'
+    },
+
+]
+
 
 #Back End Route
 
@@ -7,14 +26,14 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    #HTML Header Tag
-    return "<h1>Home Page<h1>"
+    #Takes in posts data
+    return render_template('home.html', posts=posts)
 
 #About Page Route
 @app.route("/about")
 def about():
     #HTML Header Tag
-    return "<h1>About Page<h1>"
+    return render_template('about.html', title='About')
 
 #Debug Mode if ran via python
 if __name__ == '__main__':
