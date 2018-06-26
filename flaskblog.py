06745ab4d2c97d61bbb6d19e46c38b966bcaa99e
Deval Parikh <devalpp@gmail.com>
2018-06-25 20:27:56 -0400

    About Page init

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+#Back End Route
+
+#Home Page Route
 @app.route("/")
 def hello():
-    return "Hello World!"
+    #HTML Header Tag
+    return "<h1>Home Page<h1>"
+
+#Home Page Route
+@app.route("/about")
+def about():
+    #HTML Header Tag
+    return "<h1>About Page<h1>"
+
+#Debug Mode if ran via python
+if __name__ == '__main__':
+    app.run(debug=True)
